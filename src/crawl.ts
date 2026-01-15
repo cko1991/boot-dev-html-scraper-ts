c1bb8fdef9c8de84d9177b5416b3d943bacbff52
Chaitanya <chaitanya.kochhar@gmail.com>
2026-01-15 22:22:42 +0530

    WIP scraping

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(url : string) : string
 {
     const urlObj = new URL(url);
@@ -7,4 +9,15 @@ export function normalizeURL(url : string) : string
     }
     normalizedUrl = normalizedUrl.trim();
     return normalizedUrl.toLowerCase();
+}
+
+export function getH1FromHTML(html : string) : string
+{
+    const dom = new JSDOM(html);
+    const h1s = dom.window.document.querySelectorAll('h1');
+    if (h1s.length === 0) {
+        return '';
+    }
+
+    return h1s[0].
 }
